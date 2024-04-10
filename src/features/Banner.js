f69c3411b82b98db469c08f0da8dd26412d1058b
Chukwuma Ibezim <123107253+cibezim@users.noreply.github.com>
2024-04-10 14:47:37 -0400

    fix: fix Banner.js and Nav.js build errors

--- src/features/Banner.js
+++ src/features/Banner.js
@@ -1,38 +1,34 @@
-import React, { useEffect,useState } from 'react'
-import "./Banner.css"
+import React, { useEffect, useState } from 'react';
+import './Banner.css';
 import axios from './axios';
 import requests from './Request';
 
 function Banner() {
+    const [movie, setMovie] = useState([]);
 
-    const [movie,setMovie] = useState([]);
-
-    useEffect(() =>{
-        async function fetchData(){
+    useEffect(() => {
+        async function fetchData() {
             const request = await axios.get(requests.fetchTopRated);
             setMovie(
                 request.data.results[
                     Math.floor(Math.random() * request.data.results.length - 1)
-                    ]
+                ]
             );
             return request;
         }
         fetchData();
     }, []);
 
-    console.log(movie)
+    console.log(movie);
 
-    //  function truncate(string, n){
-    //      return string?.length > n ? string.substr(0, n - 1) + '...' : string;
-
-    //  }
     return (
-        <header className="banner" style={{
-            backgroundSize: "cover",
-            backgroundImage:`url("https://image.tmdb.org/t/p/original/${movie?.
-                backdrop_path}")`,
-            backgroundPosition: "center center",
-        }}
+        <header
+            className="banner"
+            style={{
+                backgroundSize: 'cover',
+                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+                backgroundPosition: 'center center',
+            }}
         >
             <div className="banner_contents">
                 <h1 className="banner_title">
@@ -41,21 +37,13 @@ function Banner() {
                 <div className="banner_buttons">
                     <button className="banner_button">Play</button>
                     <button className="banner_button">My list</button>
-
-
                 </div>
-                <h1 className="banner_description">
-                    {movie?.overview}
-                </h1>
-                {/* <h2>{movie?.}</h2> */}
-
+                <h1 className="banner_description">{movie?.overview}</h1>
             </div>
 
-            <div className="banner--fadeBottom"/>
-
-
+            <div className="banner--fadeBottom" />
         </header>
-    )
+    );
 }
 
-export default Banner
+export default Banner;
